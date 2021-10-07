dcd05870c6b5b9bad8bf30255864d8cadfa34bd4
Nicolas Horst <Nicolas.Horst@rwth-aachen.de>
2021-10-07 19:05:37 +0200

    fix control and alt keys

--- keyboards/kyria/keymaps/NeoQwertz/keymap.c
+++ keyboards/kyria/keymaps/NeoQwertz/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_TAB,                  KC_Q,   KC_W,     KC_E,                 KC_R,                  KC_T,                                                                           KC_Y,                  KC_U,                  KC_I,    KC_O,    KC_P,    DE_UDIA,
       KC_ESC,                  KC_A,   KC_S,     KC_D,                 KC_F,                  KC_G,                                                                           KC_H,                  KC_J,                  KC_K,    KC_L,    DE_ODIA, DE_ADIA,
-      MOD_LCTL,                KC_Z,   KC_X,     KC_C,                 KC_V,                  KC_B,               KC_LSFT,   KC_LSFT, KC_LSFT,            KC_LSFT,            KC_N,                  KC_M,                  KC_COMM, KC_DOT,  DE_SS,   MOD_LCTL,
-                                       MOD_LALT, MT(MOD_LGUI, KC_BSPC), MT(MOD_LSFT, KC_SPC),  LT(_LOWER, KC_TAB), LT(_RAISE, KC_ENT), LT(_RAISE, KC_ENT), LT(_LOWER, KC_TAB), MT(MOD_RSFT, KC_SPC),  MT(MOD_RGUI, KC_DEL), MOD_RALT
+      KC_LCTL,                KC_Z,    KC_X,     KC_C,                 KC_V,                  KC_B,               KC_LSFT,   KC_LSFT, KC_LSFT,            KC_LSFT,            KC_N,                  KC_M,                  KC_COMM, KC_DOT,  DE_SS,   KC_RCTL,
+                                       KC_LALT, MT(MOD_LGUI, KC_BSPC), MT(MOD_LSFT, KC_SPC),  LT(_LOWER, KC_TAB), LT(_RAISE, KC_ENT), LT(_RAISE, KC_ENT), LT(_LOWER, KC_TAB), MT(MOD_RSFT, KC_SPC),  MT(MOD_RGUI, KC_DEL), KC_RALT
     ),
 /*
  * Lower Layer: Symbols
