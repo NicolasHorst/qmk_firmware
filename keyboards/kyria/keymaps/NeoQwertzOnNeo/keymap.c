ccca96423960d125f3de67590bada2e028c9e153
Nicolas Horst <Nicolas.Horst@posteo.de>
2021-12-06 21:00:49 +0100

    add command and option

--- keyboards/kyria/keymaps/NeoQwertzOnNeo/keymap.c
+++ keyboards/kyria/keymaps/NeoQwertzOnNeo/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |  Ctrl  |   Y  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  ; | . :  |   ß  | LShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      | Bksp | Space| Tab  | Enter|  | Enter| Tab  | Space| Del  |      |
+ *                        |      |      | Space| Tab  | Enter|  | Enter| Tab  | Space|      |      |
  *                        |Option| Cmd  |LShift| Lower| Raise|  | Raise| Lower|RShift| Cmd  |Option|
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,                 KC_Q,   KC_W,     KC_E,                  KC_R,                  KC_T,                                                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, DE_UDIA,
       KC_ESC,                 KC_A,   KC_S,     KC_D,                  KC_F,                  KC_G,                                                                    KC_H,    KC_J,    KC_K,    KC_L, DE_ODIA, DE_ADIA,
       KC_LCTL,                KC_Z,   KC_X,     KC_C,                  KC_V,                  KC_B,            KC_LSFT,   KC_LSFT, KC_LSFT,            KC_LSFT,        KC_N,  KC_M,    KC_COMM, KC_DOT,   DE_SS, KC_RCTL,
-                                    _______,  _______, MT(MOD_LSFT, KC_SPC),    LT(_LOWER, KC_TAB), LT(_RAISE, KC_ENT), LT(_RAISE, KC_ENT), LT(_LOWER, KC_TAB), MT(MOD_RSFT, KC_SPC),  _______, _______
+                                   KC_LOPT,  KC_LCMD, MT(MOD_LSFT, KC_SPC),    LT(_LOWER, KC_TAB), LT(_RAISE, KC_ENT), LT(_RAISE, KC_ENT), LT(_LOWER, KC_TAB), MT(MOD_RSFT, KC_SPC),  KC_LCMD, KC_LOPT
     ),
 /*
  * Lower Layer: Symbols
